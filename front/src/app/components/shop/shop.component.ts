d96b193605495a6d388ca814b4a9dd3ead520141
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2023-09-20 15:37:26 +0700

    расчет ежемесячного платежа

--- front/src/app/components/shop/shop.component.ts
+++ front/src/app/components/shop/shop.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RequestInputComponent } from '../my-requests/request-input/request-input.component';
 import { MatDialog } from '@angular/material/dialog';
 import { InputDialogModel, InputDialogType } from 'src/app/shared/models/input-dialog-type';
-import { LendingType } from 'src/app/shared/models/lending-type';
+import { LendingType, LendingTypeExt } from 'src/app/shared/models/lending-type';
 
 @Component({
   selector: 'shop',
@@ -11,13 +11,37 @@ import { LendingType } from 'src/app/shared/models/lending-type';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent {
+  LendingTypeExt = LendingTypeExt;
+  
   form: FormGroup;
+  maxYearCount = 30;
 
   constructor(
     private fb: FormBuilder,
     private dialog: MatDialog) {
     this.form = this.fb.group({
-      amount: [null, Validators.required]
+      lendingType: LendingType.CONSUMER,
+      amount: [null, Validators.required],
+      yearCount: 1,
+      monthlyPaymentAmount: null
+    });
+    this.form.controls.monthlyPaymentAmount.disable();
+    this.form.controls.lendingType.valueChanges.subscribe((type) => {
+      if (type == LendingType.MORTGAGE) {
+        this.form.controls.yearCount.setValue(30);
+      } else {
+        this.form.controls.yearCount.setValue(5);
+      }
+    });
+
+    this.form.valueChanges.subscribe(() => {
+      if (this.form.invalid) return;
+
+      const s = this.form.controls.amount.value ?? 0;
+      const n = this.form.controls.yearCount.value * 12;
+      const p = 8.4 / (12 * 100);
+      const x = s * (p + p / ((1+p)**n - 1));
+      this.form.controls.monthlyPaymentAmount.setValue(x);
     });
   }
 
@@ -26,7 +50,10 @@ export class ShopComponent {
       title: 'Новая заявка',
       applyButton: 'Создать',
       dialogType: InputDialogType.Create,
-      data: { creditAmount: this.form.controls.amount.value, lendingType: LendingType.CONSUMER }
+      data: { 
+        creditAmount: this.form.controls.amount.value,
+        lendingType: this.form.controls.lendingType.value
+      }
     })}).afterClosed().toPromise();
   }
 }
