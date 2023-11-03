211b73fd6be9468cc54b5f9b5f2789f16fc9728c
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2023-11-03 20:56:02 +0700

    FIX

--- widget/src/app/shared/services/widget-service.ts
+++ widget/src/app/shared/services/widget-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { SessionService } from './session.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +8,14 @@ import { SessionService } from './session.service';
 export class WidgetService {
   private baseUrl = location.protocol + '//' + location.host.slice(0, location.host.indexOf(':')) + ':8081';
 
-  constructor(
-    private http: HttpClient,
-    private sessionService: SessionService) {
+  constructor(private http: HttpClient) {
   }
 
   getWidget(): Observable<Object> {
-    return this.http.get(this.baseUrl + '/api/v1/widget/getFirst', {
-      headers: {Authorization: 'Bearer ' + this.sessionService.getToken()}
-    });
+    return this.http.get(this.baseUrl + '/api/v1/widget/getFirst');
   }
 
   getTheme(id: number): Observable<Object> {
-    return this.http.get(this.baseUrl + '/api/v1/widget/themes/' + id, {
-      headers: {Authorization: 'Bearer ' + this.sessionService.getToken()}
-    });
+    return this.http.get(this.baseUrl + '/api/v1/widget/themes/' + id);
   }
 }
